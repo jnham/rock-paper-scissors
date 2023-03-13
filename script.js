0a920581aaa8e221ca7504cc9bb3e1ad08e28fbf
jnham <108989781+jnham@users.noreply.github.com>
2023-03-13 00:27:37 -0400

    Add responsive UI to rps game
    Images included.
    First to 5 wins.
    Restart button added.
    Scoreboard added.
    Restart after 5.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let wins = 0;
+let losses = 0;
+const playerChoice = document.getElementById("playerChoice");
+const computerChoice = document.getElementById("computerChoice");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const resultImage = document.getElementById("resultImage");
+const statusText = document.getElementById("status-text");
+const gameIcons = document.getElementById("icons");
+const resetIcon = document.getElementById("reset");
+const weaponText = document.getElementById("weapon-text");
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
@@ -12,35 +24,110 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let result = "";
+    
+
+    if (computerSelection === "rock") {
+        computerChoice.src="images/rock.png";
+    }
+    else if (computerSelection === "paper") {
+        computerChoice.src="images/paper1.png";
+    }
+    else {
+        computerChoice.src="images/scissors.png";
+    }
+
+    if (playerSelection === "rock") {
+        playerChoice.src="images/rock.png";
+    }
+    else if (playerSelection === "paper") {
+        playerChoice.src="images/paper1.png";
+    }
+    else {
+        playerChoice.src="images/scissors.png";
+    }
+
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        result = "It's a tie!";
+        resultImage.src="images/cattie.jpg";
+        statusText.textContent = "A tie?... Interesting... We go again!";
     }
     else if (playerSelection === "rock") {
         if (computerSelection === "paper") {
-            return "You lose! Paper beats Rock";
+            result = "You lose! Paper beats Rock";
+            resultImage.src="images/rockcatwins.jpg";
+            statusText.textContent = "I have mastered your \"Rock\". You lose!";
         }
         else {
-            return "You win! Rock beats Scissors";
+            result = "You win! Rock beats Scissors";
+            resultImage.src="images/rockcatloses.png";
+            statusText.textContent = "Curses, foiled by rocks. You win!";
         }
     }
     else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
-            return "You lose! Scissors beats Paper";
+            result = "You lose! Scissors beats Paper";
+            resultImage.src="images/papercatwins.png";
+            statusText.textContent = "Your paper only makes me stronger. You lose!";
         }
         else {
-            return "You win! Paper beats Rock";
+            result = "You win! Paper beats Rock";
+            resultImage.src="images/papercatloses.jpg";
+            statusText.textContent = "Ahhh!! Two-ply! My weakness! You win!";
         }
     }
     else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            return "You lose! Rock beats Scissors";
+            result = "You lose! Rock beats Scissors";
+            resultImage.src="images/scissorscatwins.jpg";
+            statusText.textContent = "Your shears do not affect me. You lose!";
         }
         else {
-            return "You win! Scissors beats Paper";
+            result = "You win! Scissors beats Paper";
+            resultImage.src="images/scissorscatloses.png";
+            statusText.textContent = "Your blade is sharp. You win!";
         }
+    }
+
+    if (result.includes("win")) {
+        wins ++;
+    }
+    else if (result.includes("lose")) {
+        losses ++;
+    }
+
+    playerScore.textContent = wins;
+    computerScore.textContent = losses;
+
+    console.log(result);
+
+    if (wins >= 5) {
+        resultImage.src="images/catloses.png";
+        statusText.textContent = "You win!!! How could this be??";
+        gameIcons.style.display = "none";
+        resetIcon.style.display = "block";
+        weaponText.textContent = "Play again?"
+    }
+    else if (losses >= 5) {
+        resultImage.src="images/catwins.png";
+        statusText.textContent = "You lose! That was fun :)";
+        gameIcons.style.display = "none";
+        resetIcon.style.display = "block";
+        weaponText.textContent = "Play again?"
     }
 }
 
+function reset() {
+    wins = 0;
+    losses = 0;
+    playerScore.textContent = wins;
+    computerScore.textContent = losses;
+    resultImage.src="images/angry-cat.jpg";
+    statusText.textContent = "Rock Paper Scissors... First to 5 wins...";
+    gameIcons.style.display = "flex";
+    resetIcon.style.display = "none";
+    weaponText.textContent = "Choose your weapon!"
+}
 function game() {
     let wins = 0;
     let losses = 0;
@@ -78,7 +165,6 @@ function game() {
                 playerSelection = playerSelection.toLowerCase();
             }
         }
-        let computerSelection = getComputerChoice();
         let result = playRound(playerSelection, computerSelection);
         console.log(result);
         alert(result);
